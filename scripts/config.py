0a82096418bb29ceb5dbcf4b971d9aa548d86358
perinchiang <perinchiang@gmail.com>
2026-01-06 15:21:40 +0800

    Create config.py

---
+++ scripts/config.py
@@ -0,0 +1,14 @@
+# Notion Property Types
+RICH_TEXT = "rich_text"
+URL = "url"
+RELATION = "relation"
+NUMBER = "number"
+DATE = "date"
+FILES = "files"
+STATUS = "status"
+TITLE = "title"
+SELECT = "select"
+MULTI_SELECT = "multi_select"
+
+# Icons (用来关联年/月/周时的默认图标，可保持原样)
+TARGET_ICON_URL = "https://www.notion.so/icons/target_red.svg"
